51ec356707b62fc7b6af56ff873cc7ee971393ca
jasondoss <jason.j.doss@gmail.com>
2022-11-22 21:58:09 -0800

    added Api key, added cards to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,8 @@
+var searchFormEl = document.querySelector("#search-city");
+var resultTextEl = document.querySelector("#currentCityName");
+var currentCityName;
 var apiKey = "cc639e256b8793492dc1bf5097da5449
-";
+var weather = [];
+var cityList = [];
+
+
